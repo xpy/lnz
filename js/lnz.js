33b853aa9798ff3931dcb9c7211c4a6606e27746
xpy <pavloschris@gmail.com>
2012-12-18 17:01:42 +0200

    fixed all directions

--- js/lnz.js
+++ js/lnz.js
@@ -3,6 +3,7 @@ var lnz = function(){
 };
 
 var $lnzCanvas = $( '#lnz-canvas' );
+
 var lnzId = function(){
 	var idSeed = 0;
 	return function(){
@@ -16,37 +17,47 @@ var lnzLine = function( start, end ){
 
 	var $start = ( typeof start == 'object' ) ? start : null;
 	var $end = ( typeof end == 'object' ) ? end : null;
+	var zeroFunc = function(){return 0;}
+	var startW = ( typeof start == 'object' ) ? function(){ return $start.outerWidth(); } : zeroFunc;
+	var startH = ( typeof start == 'object' ) ? function(){ return $start.outerHeight(); } : zeroFunc;
+	var endW = ( typeof end == 'object' ) ? function(){ return $end.outerWidth(); } : zeroFunc;
+	var endH = ( typeof end == 'object' ) ? function(){ return $end.outerHeight(); } : zeroFunc;
 	var canvasW = $lnzCanvas.width();
 	var canvasH = $lnzCanvas.height();
 
-	var staticStartPos = function(){ return start;}
-	var staticEndPos = function(){ return end; }
+	var staticStartPos = function(){ return start;};
+	var staticEndPos = function(){ return end; };
 
 	var dynamicStartPos = function(){
-		return [$start.offset().left, $start.offset().top]
-	}
-	var dynamicEndPos = function(){
-		return [$end.offset().left, $end.offset().top]
-	}
+		return [$start.offset().left + (startW()/2), $start.offset().top + (startH()/2)]
+	};
 
-	var starPos = ( typeof start == 'object' ) ? dynamicStartPos : staticStartPos;
+	var dynamicEndPos = function(){
+		return [$end.offset().left + (endW()/2), $end.offset().top  + (endH()/2)]
+	};
+
+	var startPos = ( typeof start == 'object' ) ? dynamicStartPos : staticStartPos;
 	var endPos = ( typeof end == 'object' ) ? dynamicEndPos : staticEndPos;
 
 
 	T.applyPos = function(){
+		var tmpStartPos = startPos();
+		var tmpEndPos =  [  endPos()[0] - tmpStartPos[0],endPos()[1] -tmpStartPos[1]];
+		if(tmpEndPos[0]*tmpEndPos[1] < 0){T.$this.addClass('rev');} else {T.$this.removeClass('rev');}
 		T.$this.css( {
-			left  :starPos()[0],
-			top   :starPos()[1],
-			right :canvasW - endPos()[0],
-			bottom:canvasH - endPos()[1]
+			left  :tmpStartPos[0]+(tmpEndPos[0]>0?0:tmpEndPos[0]),
+			top   :tmpStartPos[1]+(tmpEndPos[1]>0?0:tmpEndPos[1]),
+			width :Math.abs(tmpEndPos[0]),
+			height:Math.abs(tmpEndPos[1])
 		} );
-	}
+
+	};
 
 	T.id = lnzId();
 	$lnzCanvas.append( '<div class="lnz" id="lnz-' + T.id + '"  ></div>' );
-	T.$this = $( '#lnz-' + T.id  );
+	T.$this = $( '#lnz-' + T.id );
 
-	T.a = window.setInterval(T.applyPos, 20 );
+	T.a = window.setInterval( T.applyPos, 20 );
 
 };
 
